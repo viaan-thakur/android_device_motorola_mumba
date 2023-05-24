b4cf56718779fb82f59ddf10c2412e6e2b136edf
Shashank Sivakumar <quic_shassiva@quicinc.com>
2023-05-24 18:14:55 +0530

    mumba: Enable AVF
    Change-Id: Iaa373c0676219a724e2033d9a7863dfdd7b7504b

--- device.mk
+++ device.mk
@@ -6,6 +6,11 @@
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 
+# pKVM
+$(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk)
+
+PRODUCT_BUILD_PVMFW_IMAGE := true
+
 # Shipping API
 BOARD_SHIPPING_API_LEVEL := 202404
 PRODUCT_SHIPPING_API_LEVEL := 36
