56758a54fd69739f07398ad9b9eecd7e937b7c8b
jmpfbmx <jmpf.bmx@gmail.com>
2025-01-03 14:16:38 +0100

    mumba: Inherit QCOM and motorola sepolicy
    Change-Id: Ice47cd8f3809f444d867de1336449dc9e36ce173
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,5 +118,9 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
+# SEPolicy
+include device/qcom/sepolicy_vndr/SEPolicy.mk
+include hardware/motorola/sepolicy/qti/SEPolicy.mk
+
 # Include the proprietary files BoardConfig.
 include vendor/motorola/mumba/BoardConfigVendor.mk
