ea8869b5d7617d8dce6371740183d8245187f1ee
jmpfbmx <jmpf.bmx@gmail.com>
2025-01-03 14:03:25 +0100

    mumba: Add arch, bootloader and platform flags
    Change-Id: Ie0ca1d06a57a715867ceb4954cc37248cd099081
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,5 +5,19 @@
 
 DEVICE_PATH := device/motorola/mumba
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := kryo300
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := parrot
+
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := parrot
+
 # Include the proprietary files BoardConfig.
 include vendor/motorola/mumba/BoardConfigVendor.mk
