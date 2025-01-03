83f97ab1ed759eca5fee9b0cb5240a421a1df15d
jmpfbmx <jmpf.bmx@gmail.com>
2025-01-03 14:15:40 +0100

    mumba: Add recovery flags
    Change-Id: I8fde10f1a50317950597a63249112e0154897e1a
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,5 +111,12 @@ $(foreach p, $(call to-upper, $(BOARD_MOT_DP_GROUP_PARTITION_LIST)), \
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := parrot
 
+# Recovery
+TARGET_RECOVERY_UI_MARGIN_HEIGHT := 90
+BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
 # Include the proprietary files BoardConfig.
 include vendor/motorola/mumba/BoardConfigVendor.mk
