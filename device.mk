0ad644f7b57363656049cdde530736c67d9eae86
LuK1337 <priv.luk@gmail.com>
2025-01-08 22:02:26 +0100

    mumba Import fstab from stock
    Change-Id: Iafff80ce70f2c3ba666c741ccd7ec5f70bac5241

--- device.mk
+++ device.mk
@@ -51,6 +51,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2024-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2024-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# Init
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    fstab.qcom.zram \
+    fstab.qcom.vendor_ramdisk
+
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 
