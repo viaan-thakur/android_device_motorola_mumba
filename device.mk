1236bbbe2de6d2337409bb97dc055e6845eeea68
LuK1337 <priv.luk@gmail.com>
2025-01-17 10:27:09 +0100

    mumba Build sensors HAL
    Change-Id: I0234a4241821af5ee12e70d1f33b80719e1a61bc

--- device.mk
+++ device.mk
@@ -266,6 +266,11 @@ BOARD_SHIPPING_API_LEVEL := 202404
 PRODUCT_SHIPPING_API_LEVEL := 36
 
 # Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors-service.multihal \
+    sensors.dynamic_sensor_hal \
+    sensors.moto_ext
+
 PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_parrot/android.hardware.sensor.accelerometer.xml \
         frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_parrot/android.hardware.sensor.barometer.xml \
