f5a9df0ab35add18b4a4bf2f633c88a2d22b192a
LuK1337 <priv.luk@gmail.com>
2025-01-17 10:26:35 +0100

    mumba Build NFC
    Change-Id: Ib9e6964437d093628cf41739d3c8af916c0fdc24

--- device.mk
+++ device.mk
@@ -231,6 +231,17 @@ PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/common/codec2/service/1.0/c2audio.vendor.ext-arm64.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/c2audio.vendor.ext-arm64.policy
 
 # NFC
+TARGET_USES_ST_NFC := true
+
+PRODUCT_PACKAGES += \
+    android.hardware.nfc-service.st
+    Tag
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.nfc_cfg.config_file_name=libnfc-nci-st.conf
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.nfc.st.multisim_fw_config=true
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
