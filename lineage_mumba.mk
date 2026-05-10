989e051ec8f49f4e64d2d84b005090f723f958ba
zaraknyu <razenxizu@gmail.com>
2026-05-10 12:42:20 +0000

    mumba: Update from WWAAS36V.48-12-ST12.1-5b6d8
    Signed-off-by: zaraknyu <razenxizu@gmail.com>

--- lineage_mumba.mk
+++ lineage_mumba.mk
@@ -22,8 +22,8 @@ PRODUCT_MODEL := motorola moto g57 power
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="mumba_g-user 16 W1WAAS36M.48-12-ST12.1 768e29 release-keys" \
-    BuildFingerprint=motorola/mumba_g/mumba:16/W1WAAS36M.48-12-ST12.1/768e29:user/release-keys \
+    BuildDesc="mumba_g-user 16 W1WAA36M.48-12-ST12.1 83f8c release-keys" \
+    BuildFingerprint=motorola/mumba_g/mumba:16/W1WAA36M.48-12-ST12.1/83f8c:user/release-keys \
     DeviceName=mumba_g \
     DeviceProduct=mumba_g \
     SystemDevice=mumba \
