f1cbc59c35d09cf7b86a46b54b1f7138ecfe2c92
zaraknyu <razenxizu@gmail.com>
2026-05-12 16:25:12 +0000

    mumba: Lets go enforcing kernel
    USE sepolicies from libperfmgr, qcom common, motorola hardware
    
    Signed-off-by: zaraknyu <razenxizu@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,8 +86,7 @@ BOARD_BOOTCONFIG := \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3 \
     androidboot.hypervisor.protected_vm.supported=true \
-    androidboot.vendor.qspa=true \
-    androidboot.selinux=permissive
+    androidboot.vendor.qspa=true
 
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
