9ea4173a7191ce40b4c5cefff1064af59f4d5f7a
zaraknyu <razenxizu@gmail.com>
2026-05-24 12:43:28 +0000

    mumba: Switch to Stock DOLBY ATMOS

--- device.mk
+++ device.mk
@@ -98,10 +98,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
-# Dolby
-PRODUCT_PACKAGES += \
-    DolbyAtmos
-
 TARGET_EXCLUDES_AUDIOFX := true
 
 # Bluetooth
